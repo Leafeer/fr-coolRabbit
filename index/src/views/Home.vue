967a5c7b5238ba472db827027c64031e1c053217
leafeer <leafeer@163.com>
2020-11-09 12:38:36 +0800

    【前端】更新前端代码

--- index/src/views/Home.vue
+++ index/src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
+    <nav-menu></nav-menu>
     <router-view/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import NavMenu from '../components/common/NavMenu'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components:{
+    NavMenu
+  }
 }
 </script>
