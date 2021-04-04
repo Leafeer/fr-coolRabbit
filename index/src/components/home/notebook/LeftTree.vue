0d86a5e3ed2692a0cbc71b06c0e0ef1d86dcee5d
leafeer <leafeer@163.com>
2021-04-04 15:44:05 +0800

    文件夹树

--- index/src/components/home/notebook/LeftTree.vue
+++ index/src/components/home/notebook/LeftTree.vue
@@ -3,7 +3,29 @@
     left
     <div @click="_getAllFolder">tree</div>
     <div>
-      <el-tree :data="oTreeData" :props="defaultProps"></el-tree>
+      <el-tree
+        :data="oTreeData"
+        show-checkbox
+        node-key="id"
+        default-expand-all
+        :expand-on-click-node="false"
+      >
+        <span class="custom-tree-node" slot-scope="{ node, oTreeData }">
+          <span>{{ node.label }}</span>
+          <span>
+            <el-button type="text" size="mini" @click="() => append(oTreeData)">
+              Append
+            </el-button>
+            <el-button
+              type="text"
+              size="mini"
+              @click="() => remove(node, oTreeData)"
+            >
+              Delete
+            </el-button>
+          </span>
+        </span>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -16,13 +38,20 @@ export default {
     return {
       oTreeData: [
         {
+          id: 1,
           label: "一级 1",
           children: [
             {
+              id: 4,
               label: "二级 1-1",
               children: [
                 {
+                  id: 9,
                   label: "三级 1-1-1",
+                },
+                {
+                  id: 10,
+                  label: "三级 1-1-2",
                 },
               ],
             },
@@ -42,13 +71,27 @@ export default {
           res.data.forEach((ele) => {
             console.log(ele.folderName);
             let oTree = {
-                children: [],
-                label: ele.folderName,
-            }
-            this.oTreeData.push(oTree)
+              children: [],
+              label: ele.folderName,
+            };
+            this.oTreeData.push(oTree);
           });
         }
       });
+    },
+    append(data) {
+      const newChild = { id: id++, label: "testtest", children: [] };
+      if (!data.children) {
+        this.$set(data, "children", []);
+      }
+      data.children.push(newChild);
+    },
+
+    remove(node, data) {
+      const parent = node.parent;
+      const children = parent.data.children || parent.data;
+      const index = children.findIndex((d) => d.id === data.id);
+      children.splice(index, 1);
     },
   },
 };
